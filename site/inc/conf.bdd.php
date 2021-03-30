b9fe6652c1a3e6af190a82faa803c59ff8fc03ee
ExoStarfall <73234212+ExoStarfall@users.noreply.github.com>
2021-03-30 13:47:34 +0200

    Add files via upload

---
+++ site/inc/conf.bdd.php
@@ -0,0 +1,21 @@
+<?php
+
+  /*define("DB_SERVER",   "localhost"); //Ajout localhost
+  define("DB_DATABASE", "country"); // Ajout du nom de la db
+  define("DB_USERNAME", "XXXXXX"); // Ajout du username
+  define("DB_PASSWORD", "XXXXXX"); // Ajout du mot de passe */
+
+
+  // ON A MIS LES CONSTANTES 
+
+  const DB_SERVER    = "localhost";
+  const DB_DATABASE    = "country";
+  const DB_USERNAME   = "XXXXXXXXXX";
+  const DB_PASSWORD    = "XXXXXXX";
+
+  const DB_DRIVER  = "mysql";
+  const DB_CHARSET = "utf8mb4";
+
+
+
+?>
